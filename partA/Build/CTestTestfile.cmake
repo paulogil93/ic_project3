23d688fe111451710864353ef7fbe89aa50dfc6a
Hugo Moinheiro <hugo.moinheiro@ua.pt>
2022-02-05 13:42:34 +0000

    cleanup

--- partA/Build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hugo/ic/proj3/partA
-# Build directory: /home/hugo/ic/proj3/partA/Build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
